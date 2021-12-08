a9d6f5292f096f5f245734728aec6737ea0e2f0f
yakup <yakupkayaa@gmail.com>
2021-12-09 01:44:28 +0300

    r1

---
+++ setecion1.sql
@@ -0,0 +1,8 @@
+SELECT name FROM tracks;
+SELECT Composer,name FROM tracks;
+SELECT * FROM tracks;
+SELECT DISTINCT Composer FROM tracks;
+SELECT DISTINCT Composer FROM tracks;
+SELECT DISTINCT AlbumId, MediaTypeId FROM tracks;
+SELECT name FROM tracks where Composer="Jimi Hendrix";
+SELECT * from invoices where total>10;
